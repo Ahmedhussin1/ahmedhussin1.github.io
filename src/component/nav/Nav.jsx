b55aad695f2c52e0557476a9154165d4348146c8
YE <ye@pop-os.localdomain>
2023-02-08 00:42:07 +0200

    navbar finished,  functionality added using usestate

--- src/component/nav/Nav.jsx
+++ src/component/nav/Nav.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import { BiBookAlt } from "react-icons/bi";
+import { RiServiceFill } from "react-icons/ri";
+import { AiOutlineMessage } from "react-icons/ai";
+import { useState } from 'react';
+
+
 function Nav() {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
-  )
+    <nav>
+      <a
+        href="#"
+        className={activeNav === "#" ? "active" : ""}
+        onClick={() => setActiveNav("#")}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience "
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BiBookAlt />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <RiServiceFill />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <AiOutlineMessage />
+      </a>
+    </nav>
+  );
 }
 
 export default Nav
